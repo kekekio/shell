7aefedb6d88eb7307f54c94f429053bc4539dc2b
kio ino <tg:@kekekio>
2024-06-07 15:48:29 +0300

    from now it used as env

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 # \brief setup all envs listed here
 #source 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "$SCRIPT_DIR/*.env"
+source "$SCRIPT_DIR"/*.env
 
